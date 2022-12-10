da012bb1a8d61fbdfc772f15eec9c14cf85f56f8
Mark <super16mail@gmail.com>
2022-12-11 00:42:22 +0300

    Configure deploying with GitHub actions

---
+++ scripts/gh-pages-deploy.mjs
@@ -0,0 +1,17 @@
+
+import { execa } from 'execa';
+
+(async () => {
+  try {
+    await execa('git', ['checkout', '--orphan', 'gh-pages']);
+    await execa('npm', ['run', 'build']);
+    await execa('git', ['--work-tree', 'dist', 'add', '--all']);
+    await execa('git', ['--work-tree', 'dist', 'commit', '-m', 'gh-pages deployment']);
+    await execa('git', ['push', 'origin', 'HEAD:gh-pages', '--force']);
+    await execa('rm', ['-r', 'dist']);
+    await execa('git', ['checkout', '-f', 'main']);
+    await execa('git', ['branch', '-D', 'gh-pages']);
+  } catch (e) {
+    process.exit(1);
+  }
+})();
